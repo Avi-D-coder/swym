a8259fee98c88f5df1bf1b451668ecb87ad5fb64
tyler <tyler@brainiumstudios.com>
2019-07-15 20:32:39 -0700

    fix CI

--- ci/htm.sh
+++ ci/htm.sh
@@ -8,7 +8,7 @@ export RUSTFLAGS="-D warnings"
 
 cargo check --no-default-features
 cargo check --benches --bins --examples --tests
-cargo check --features rtm --benches --bins --examples --tests
+cargo check --features nightly --benches --bins --examples --tests
 cargo check --features htm --benches --bins --examples --tests
 ./x.py test
 ./x.py test --release -- --nocapture
